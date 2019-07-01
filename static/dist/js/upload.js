47b7abf1433b2ba6a2c30efbd4337aa109b7f7e3
d0542218 <32465113+d0542218@users.noreply.github.com>
2019-07-01 10:31:53 +0800

    Add files via upload
    dist upload

---
+++ static/dist/js/upload.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('#step-1-next').click(function() {
+        var xxx = ...
+        $.ajax({
+            type: "GET", //GET POST
+            url: "/storedata",
+            data: {
+                pic = xxx
+            },
+            dataType: 'JSON',
+            success: function(data) {
+                console.log(data)
+                return data;
+            },
+            error: function(msg) {
+                return null;
+            }
+            127.0 .0 .1: 8800 / storedata ? id = 1 & name = yuhaoting
+        })
+    })
+
+})
