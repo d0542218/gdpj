6739c8418c903b34e069ab0d2ccc7cf99864f32c
what211228 <what211228@gmail.com>
2019-09-29 16:23:52 +0800

    add exception

--- EsNoteScore/views.py
+++ EsNoteScore/views.py
@@ -1,11 +1,15 @@
-from django.contrib.auth.models import User
+import rest_framework_simplejwt
+from django.contrib.auth.models import User, AnonymousUser
 from django.db.models import QuerySet
 from django.http import QueryDict
+from rest_framework.exceptions import ParseError, NotFound, AuthenticationFailed
 from rest_framework.response import Response
 from EsNoteScore.models import esNote_score_model, esNote_score_pic_model
-from EsNoteScore.serializers import esNote_score_Serializer, esNote_score_pic_Serializer, UserSerializer,searchPicSerializer
+from EsNoteScore.serializers import esNote_score_Serializer, esNote_score_pic_Serializer, UserSerializer \
+    , searchPicSerializer
 from rest_framework import viewsets, authentication, permissions, status, generics, mixins
 from rest_framework_simplejwt.tokens import AccessToken
+import rest_framework_simplejwt.exceptions
 from . import permission
 
 
@@ -44,15 +48,20 @@ class EsNoteScoreViewSet(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def perform_create(self, serializer):
-        try:
+        if self.request.user == AnonymousUser():
+            try:
+                print(self.request.META)
+                token = self.request.META.get('HTTP_AUTHORIZATION').split(' ')[1]
+                access_token = AccessToken(token)
+                user = User.objects.get(id=int(access_token['user_id']))
+                serializer.save(user=user)
+            except rest_framework_simplejwt.exceptions.TokenError:
+                raise AuthenticationFailed(detail='Token is invalid or expired.')
+            except :
+                raise AuthenticationFailed(detail='Authorization is Null.')
+        else:
             serializer.save(user=self.request.user)
-            # print("hihi")
-        except:
-            token = self.request.META.get('HTTP_AUTHORIZATION', " ").split(' ')[1]
-            access_token = AccessToken(token)
-            user = User.objects.get(id=int(access_token['user_id']))
-            serializer.save(user=user)
-        pass
+
 
 
 class EsNoteScorePicViewSet(viewsets.ModelViewSet):
@@ -72,6 +81,7 @@ class EsNoteScorePicViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save()
+
 
 class upload_images(viewsets.ModelViewSet):
     authentication_classes = (authentication.TokenAuthentication,)
@@ -113,14 +123,19 @@ class upload_images(viewsets.ModelViewSet):
         QDdefaultScore.update(defaultScore)
         serializer = self.get_serializer(data=QDdefaultScore)
         serializer.is_valid(raise_exception=True)
-        try:
+        if self.request.user == AnonymousUser():
+            try:
+                print(self.request.META)
+                token = self.request.META.get('HTTP_AUTHORIZATION').split(' ')[1]
+                access_token = AccessToken(token)
+                user = User.objects.get(id=int(access_token['user_id']))
+                serializer.save(user=user)
+            except rest_framework_simplejwt.exceptions.TokenError:
+                raise AuthenticationFailed(detail='Token is invalid or expired.')
+            except :
+                raise AuthenticationFailed(detail='Authorization is Null.')
+        else:
             serializer.save(user=self.request.user)
-        except:
-            print(self.request.META)
-            token = self.request.META.get('HTTP_AUTHORIZATION').split(' ')[1]
-            access_token = AccessToken(token)
-            user = User.objects.get(id=int(access_token['user_id']))
-            serializer.save(user=user)
         self.noteID = int(dict(serializer.data).get('noteID'))
         self.return_data.update(serializer.data)
         # print(serializer.data)
@@ -128,7 +143,7 @@ class upload_images(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         # print(request.data)
-        self.order=0
+        self.order = 0
         self.temp.clear()
         self.build_default_Esnote()
         QDic = QueryDict('', mutable=True)
@@ -141,8 +156,10 @@ class upload_images(viewsets.ModelViewSet):
         # serializer = self.get_serializer(data=request.data)
         # serializer.is_valid(raise_exception=True)
         # self.perform_create(serializer)
+        if not pic.getlist('esNote_score_pic'):
+            raise ParseError(detail="esNote_score_pic is Null.")
         for i in pic.getlist('esNote_score_pic'):
-            self.order+=1
+            self.order += 1
             QDic.update({'esNote_score_pic': i})
             print("QDic")
             print(QDic)
@@ -152,16 +169,16 @@ class upload_images(viewsets.ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         self.querysetflag = 0
         self.serializerflag = 0
-        self.return_data.update({'esNote_score_pic':self.temp})
+        self.return_data.update({'esNote_score_pic': self.temp})
 
         return Response(self.return_data, status=status.HTTP_201_CREATED, headers=headers)
 
     def perform_create(self, serializer):
-        serializer.save(esNote_score=esNote_score_model.objects.get(noteID=self.noteID),order=self.order)
+        serializer.save(esNote_score=esNote_score_model.objects.get(noteID=self.noteID), order=self.order)
         self.temp.append(serializer.data)
 
 
-class model_get_pictures(viewsets.GenericViewSet,mixins.ListModelMixin):
+class model_get_pictures(viewsets.GenericViewSet, mixins.ListModelMixin):
     model = esNote_score_model
     serializer_class = searchPicSerializer
     queryset = esNote_score_model.objects.all()
@@ -175,9 +192,10 @@ class model_get_pictures(viewsets.GenericViewSet,mixins.ListModelMixin):
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
         if self.id is None:
-            pass # not done
-
+            raise ParseError(detail="Has no id.")
         serializer = self.get_serializer(queryset, many=True)
+        if not serializer.data:
+            raise NotFound(detail="id error or no data.")
         return Response(serializer.data)
 
     def get_queryset(self):
