802753faf81ece4b19878ab07bf0d450d9fb7474
what211228 <what211228@gmail.com>
2019-11-09 16:16:24 +0800

    ip fix

--- EsNoteScore/views.py
+++ EsNoteScore/views.py
@@ -601,10 +601,10 @@ class model_get_predict_pictures(viewsets.GenericViewSet, mixins.ListModelMixin)
 
     def list(self, request, *args, **kwargs):
         res = []
-        url = "http://140.134.26.63:15001/predict_by_url"
-        # url = "http://127.0.0.1:5000/predict_by_url"
-        ip = "http://140.134.26.63:18000/media/"
-        ip2 = "http://182.155.209.64:18000/media/"
+        # url = "http://140.134.26.63:15001/predict_by_url"
+        url = "http://172.23.1.1:5000/predict_by_url"
+        ip = "http://172.23.1.2:8000/media/"
+        # ip2 = "http://182.155.209.64:18000/media/"
         if self.request.user == AnonymousUser():
             try:
                 token = self.request.META.get('HTTP_AUTHORIZATION').split(' ')[1]
