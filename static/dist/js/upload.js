16fc68b892c7b2038c42f45131d169c89bd28337
what211228 <what211228@gmail.com>
2019-11-14 18:33:07 +0800

    fix url

--- static/dist/js/upload.js
+++ static/dist/js/upload.js
@@ -259,7 +259,7 @@ $("#step3Input").on("change paste", function() {
 });
 function changeOrder(id,token,new_order){
     $.ajax({
-       url:"http://127.0.0.1:8000/api/v1/change_order_of_pics_2/",
+       url:"/api/v1/change_order_of_pics_2/",
        method: "POST",
        dataType:'json',
        headers:{
